53afed553fa45c45d542c1cfd82000b24f198bcf
makenna <makenna.gordon.2023@student.lhschools.org>
2020-03-04 13:56:18 -0500

    OddnessMethodCreated

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -3,6 +3,10 @@ package com.LickingHeights;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //We want to find out if a number is even or odd
+        int number = 17;
+        System.out.println("Number is even: "+(number % 2 ==0));
+        System.out.println("Number is odd: "+ (number % 2!=0));
     }
 }
